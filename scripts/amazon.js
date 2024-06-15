f0a8ec0b24e08bb2bffef4b59806f4a60febb50e
absmlay <106912810+absmlay@users.noreply.github.com>
2024-06-15 12:47:37 +0000

    Save data and generate HTML for delivery options

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,7 @@ import { cart, addToCart, calculateCartQuantity } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+
 let productsHTML = '';
 
 products.forEach((product) => {
